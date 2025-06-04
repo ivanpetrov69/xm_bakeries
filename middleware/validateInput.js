8c56329628cb0ac54b5664d16c9e3a05fb643184
ivanpetrov69 <mpabukagreyson@gmail.com>
2025-06-04 20:52:35 +0200

    Delete middleware directory

--- middleware/validateInput.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (requiredFields) {
-  return (req, res, next) => {
-    const missing = requiredFields.filter(field => !req.body[field]);
-    if (missing.length) {
-      return res.status(400).json({ error: `Missing fields: ${missing.join(", ")}` });
-    }
-    next();
-  };
-};
